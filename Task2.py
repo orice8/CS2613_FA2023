971932ee2a7a7f51712ba7c8a3acf0f10706305f
Olivia Gerry Rice <114359587+orice8@users.noreply.github.com>
2023-09-27 14:17:59 -0300

    L6 Upload

--- Task2.py
+++ Task2.py
@@ -1,22 +1,49 @@
-import math
+def given_length(s_Input, i_Length):
+    lst_Strings = s_Input.split() # split individual wors by whitespace
+    lst_Output = [] # empty output list
+    for word in lst_Strings:
+        if len(word) == i_Length:
+            lst_Output.append(word) # save words of requested length
+    return lst_Output
 
-def absByOlivia(x):
-	if x < 0:
-		x = x * -1
-	return x
-	
-def formula(a, b):
-	return math.sqrt(absByOlivia((a-1)**5)-absByOlivia(b+1))
+def longest_word(s_Input):
+    lst_Strings = s_Input.split()
+    lst_Lengths = []
+    for word in lst_Strings:
+        lst_Lengths.append(len(word))
+    i_LengthOfLongestWord = (max(lst_Lengths))
+    lst_Output = given_length(s_Input, i_LengthOfLongestWord)
+    return lst_Output
 
-while True:
-	print("Input two values:" )
-	a = input()
-	if a == 'X':
-		print("0") # value for b
-		break
-	else:
-		a = float(a) # cast to float
-		b = float(input())
-		print(f"Result: {formula(a,b)}")
+def most_common(s_Input):
+    lst_ASCII = [0] * 26
+    for x in s_Input:
+        if x.isalpha():
+            val = ord(x) - 97
+            lst_ASCII[val] = lst_ASCII[val] + 1
+    
+    maximum = max(lst_ASCII)
+    lst_Indices = []
+    for letterCount in lst_ASCII:
+        if letterCount == maximum:
+            lst_Indices.append(lst_ASCII.index(letterCount))
+    return print("HERE: ", maximum)
 
+### TEST CASE 1 ###
 
+print(given_length("The white cat and the red fox.", 3))
+
+print(longest_word("Hello CS2613! Python is fun.") == ["CS2613!"])
+print(longest_word("Hello CS2613 - Python is fun.") == ["CS2613", "Python"])
+
+print(most_common("My name is…") == ['m'])
+print(most_common ("This is!") == ['i', 's'])
+
+### TEST CASE 2 ###
+
+f = open("/home1/ugrads/orice/CS2613/L6/L6_T4_Text.txt", "r")
+s_File = f.read()
+
+print(len(given_length(s_File, 5)))
+print(longest_word(s_File))
+#print(most_common(s_File))
