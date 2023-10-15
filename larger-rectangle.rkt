3b0030dca16e8aa8e46f3900b85385931bfaf51f
Olivia Gerry Rice <114359587+orice8@users.noreply.github.com>
2023-10-15 09:28:31 -0300

    Delete larger-rectangle.rkt

--- larger-rectangle.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-
-(define (larger-rectangle length1 width1 length2 width2)
-  (cond
-    [(equal? (* length1 width1)(* length2 width2)) 0]
-    [(> (* length1 width1)(* length2 width2)) -1]
-    [(< (* length1 width1)(* length2 width2)) 1]))
